0562afb8de7b063648575b78860ebe186d3bc172
LiuLiang <qq79402005@gmail.com>
2018-03-02 22:07:46 +0800

    submite test data

---
+++ build.bat
@@ -0,0 +1,3 @@
+python smart-config.py
+
+pause
---
+++ smart-config.py
@@ -0,0 +1,10 @@
+import os
+
+configure_dir = 'example/config/'
+excel_dir = 'example/excel/'
+output_dir = 'example/output/'
+
+def convert_xml_to_binary(config_file, xml_file, output_binary_file):
+    return
+
+convert_xml_to_binary('example/config/example.xml', 'example/output/xml/example.xml', 'example/output/xml/example.bin')
