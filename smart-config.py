d6d61db993e2245ba50e39ddd98a998caf82eb8b
LiuLiang <qq79402005@gmail.com>
2018-03-03 15:41:06 +0800

    parse config file succeed

--- smart-config.py
+++ smart-config.py
@@ -1,10 +1,46 @@
 import os
+import xml.etree.cElementTree as ET
 
-configure_dir = 'example/config/'
-excel_dir = 'example/excel/'
-output_dir = 'example/output/'
+root_path = os.getcwd() + '/'
+configure_dir = root_path + 'example/config/'
+excel_dir = root_path + 'example/excel/'
+output_dir = root_path + 'example/output/'
 
-def convert_xml_to_binary(config_file, xml_file, output_binary_file):
+def write_binary_meta_by_config_data_and_xml_data( xml_file, variable_list):
+    print(xml_file, variable_list)
+
+def convert_xml_to_binary_meta(config_file, xml_file_dir, output_binary_dir):
+    # parse config_xml iterate all beans
+    config_xml_tree = ET.parse( config_file)
+    root = config_xml_tree.getroot()
+    if root.tag != "namespace":
+        print("error: root node tag should be 'namespace'")
+        return
+
+    # record namespace
+    namespace = root.attrib["name"]
+
+    # iterate beans
+    for bean in root.iter("bean"):
+        bean_name = bean.attrib["name"]
+        bean_genxml = bean.attrib["genxml"]
+        variable_list = []
+        if bean_genxml == "client":
+            xml_file = "smart.config." + namespace + "." + bean_name + ".xml"
+
+            for variable in bean.iter("variable"):
+                variable_name = variable.attrib["name"]
+                variable_type = variable.attrib["type"]
+                variable_value = ""
+                if variable_type == "vector" :
+                    variable_value = variable.attrib["value"]
+
+                variable_info = [variable_name, variable_type, variable_value]
+                variable_list.append(variable_info)
+
+
+            write_binary_meta_by_config_data_and_xml_data( xml_file, variable_list)
+
     return
 
-convert_xml_to_binary('example/config/example.xml', 'example/output/xml/example.xml', 'example/output/xml/example.bin')
+convert_xml_to_binary_meta(configure_dir + 'example.xml', output_dir + 'xml/', output_dir + 'binary/')
